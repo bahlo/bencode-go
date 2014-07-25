1e39bb9631ede3db8f3fb42f94a56c1c0039df30
Arne Bahlo <hallo@arne.me>
2014-07-25 18:37:22 +0200

    Add test for readInt

--- bencode_test.go
+++ bencode_test.go
@@ -1,1 +1,23 @@
 package octorrent
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestReadInt(t testing.T) {
+	const in, out = "412e51", 412
+	buf := bytes.NewBufferString(in)
+	dec := decoder{
+		*buf,
+	}
+
+	out, err := dec.readInt()
+	if err != nil {
+		t.Errorf("readInt returned error: %s", err)
+	}
+
+	if out != in {
+		t.Errorf("readInt should return %i, but did return %i", in, out)
+	}
+}
