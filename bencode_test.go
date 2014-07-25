b4215da1bce479f17d18d91ec39f989d7f73e97a
Arne Bahlo <hallo@arne.me>
2014-07-25 18:45:06 +0200

    Add TestReadString, getDecoder()

--- bencode_test.go
+++ bencode_test.go
@@ -5,19 +5,38 @@ import (
 	"testing"
 )
 
-func TestReadInt(t testing.T) {
-	const in, out = "412e51", 412
-	buf := bytes.NewBufferString(in)
-	dec := decoder{
+func getDecoder(d []byte) decoder {
+	buf := bytes.NewBuffer(d)
+
+	return decoder{
 		*buf,
 	}
+}
 
-	out, err := dec.readInt()
+func TestReadInt(t *testing.T) {
+	const i, o = "412e51", 412
+	dec := getDecoder([]byte(i))
+
+	num, err := dec.readInt()
 	if err != nil {
 		t.Errorf("readInt returned error: %s", err)
 	}
 
-	if out != in {
-		t.Errorf("readInt should return %i, but did return %i", in, out)
+	if num != o {
+		t.Errorf("readInt did return %i, but should return %i", num, out)
+	}
+}
+
+func TestReadString(t *testing.T) {
+	const i, o = "4:testfoobar", "test"
+	dec := getDecoder([]byte(i))
+
+	str, err := dec.readString()
+	if err != nil {
+		t.Errorf("readString returned error: %s", err)
+	}
+
+	if str != o {
+		t.Errorf("readString returned %s, but should return %s", str, o)
 	}
 }
